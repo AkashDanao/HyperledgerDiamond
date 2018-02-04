c76537f4a21c17830ec1ee773e0d9c11b0c02a2d
AkashDanao <akash.danao@globant.com>
2018-02-04 09:37:47 +0530

    Add internet permission

--- app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/MainActivity.java
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/MainActivity.java
@@ -2,12 +2,37 @@ package com.globant.akashdanao.hyperledgerdiamond;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.globant.akashdanao.hyperledgerdiamond.Data.ApiClient;
+import com.globant.akashdanao.hyperledgerdiamond.Data.Models.Diamond;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ApiClient.getInstance().getAllDiamonds().enqueue(new Callback<List<Diamond>>() {
+            @Override
+            public void onResponse(Call<List<Diamond>> call, Response<List<Diamond>> response) {
+                Log.d(TAG, "onResponse: " + response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Diamond>> call, Throwable t) {
+                Log.d(TAG, "onfailure: " + t.toString());
+                t.printStackTrace();
+
+            }
+        });
     }
 }
