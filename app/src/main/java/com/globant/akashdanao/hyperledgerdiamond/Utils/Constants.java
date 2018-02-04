4b4484d64cfadb0295d5f0083aee86251351b0f8
AkashDanao <akash.danao@globant.com>
2018-02-04 09:40:46 +0530

    create a constant file

---
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/Utils/Constants.java
@@ -0,0 +1,10 @@
+package com.globant.akashdanao.hyperledgerdiamond.Utils;
+
+/**
+ * Created by akash.danao on 02/02/18.
+ */
+
+public class Constants {
+
+    public static final String BASE_URL = "http://192.168.232.2:8000";
+}
