8b69a2de3e02b9223f736a25bd56de7ba93e7e1e
AkashDanao <akash.danao@globant.com>
2018-02-05 12:04:35 +0530

    create add diamond activity

---
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/UI/Activities/AddDiamondActivity.java
@@ -0,0 +1,13 @@
+package com.globant.akashdanao.hyperledgerdiamond.UI.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDiamondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diamond);
+    }
+}
