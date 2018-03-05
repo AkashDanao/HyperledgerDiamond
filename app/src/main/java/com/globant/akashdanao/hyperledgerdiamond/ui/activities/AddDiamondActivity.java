7eb3772c68c73696739d6c134299946866a44771
Anand Soni <anand.soni@globant.com>
2018-03-05 22:11:30 +0530

    user interface for add new record

--- app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/activities/AddDiamondActivity.java
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/activities/AddDiamondActivity.java
@@ -1,5 +1,6 @@
 package com.globant.akashdanao.hyperledgerdiamond.ui.activities;
 
+import android.content.Context;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,36 +16,24 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class AddDiamondActivity extends AppCompatActivity {
 
-    @BindView(R.id.et_id)
-    TextView id;
-
-    @BindView(R.id.et_holder_name)
-    TextView holderName;
-
-    @BindView(R.id.et_latitude)
-    TextView latitude;
-
-    @BindView(R.id.et_longitude)
-    TextView longitude;
-
-    @BindView(R.id.et_vessel)
-    TextView vessel;
-
-    @BindView(R.id.et_timestamp)
-    TextView timeStamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_diamond);
-
+        setContentView(R.layout.activity_add_new_record);
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.bt_create)
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+   /* @OnClick(R.id.bt_create)
     public void onClickAdd() {
 
         ApiClient.instance.saveRecord(id.getText().toString(), holderName.getText().toString(), Long.parseLong(latitude.getText().toString()), Long.parseLong(longitude.getText().toString()), Long.parseLong(timeStamp.getText().toString()), vessel.getText().toString())
@@ -53,5 +42,5 @@ public class AddDiamondActivity extends AppCompatActivity {
                 .subscribe(
                         id -> Toast.makeText(this, "Record Added Successfully", Toast.LENGTH_SHORT).show(),
                         e -> Toast.makeText(this, "There is some error", Toast.LENGTH_SHORT).show());
-    }
+    }*/
 }
