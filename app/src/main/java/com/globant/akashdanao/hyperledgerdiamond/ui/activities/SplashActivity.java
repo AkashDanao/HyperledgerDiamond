a230a3dc17266ea31bf3a5d726f8166e2abecd8a
AkashDanao <akash.danao@globant.com>
2018-03-05 15:37:29 +0530

    add splash activity

---
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.globant.akashdanao.hyperledgerdiamond.ui.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.globant.akashdanao.hyperledgerdiamond.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
