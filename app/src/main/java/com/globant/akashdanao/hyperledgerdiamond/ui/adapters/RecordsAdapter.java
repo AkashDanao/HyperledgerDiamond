c0a45ee645bc5336c6436fd7548d7104efba4233
AkashDanao <akash.danao@globant.com>
2018-03-12 11:43:46 +0530

    create a two viewholders for each layout setting up viewholder is done

--- app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/adapters/RecordsAdapter.java
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/adapters/RecordsAdapter.java
@@ -5,15 +5,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.globant.akashdanao.hyperledgerdiamond.R;
 import com.globant.akashdanao.hyperledgerdiamond.data.Models.Diamond;
 import com.globant.akashdanao.hyperledgerdiamond.ui.fragments.DiamondDetailsFragment;
+import com.globant.akashdanao.hyperledgerdiamond.utils.Constants;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -21,34 +25,63 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecordsAdapter extends RecyclerView.Adapter<RecordsAdapter.ViewHolder> {
+
+public class RecordsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<Diamond> diamondList;
     private View view;
+
 
     public RecordsAdapter(List<Diamond> diamondList) {
         this.diamondList = diamondList;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.diamond_item, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == Constants.ITEM_ONE){
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.diamond_item, parent, false);
+            return new ViewHolderDefault(view);
+        }else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.diamond_item_2, parent, false);
+            return new ViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         Diamond diamond = diamondList.get(position);
-        holder.bind(diamond);
-        holder.imageViewDiamond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DiamondDetailsFragment fragment = new DiamondDetailsFragment();
-                Bundle bundle = new Bundle();
-                bundle.putString("RECORD_NUMBER", diamond.getKey());
-                fragment.setArguments(bundle);
-                loadFragment(fragment);
-            }
-        });
+        switch (holder.getItemViewType()) {
+            case Constants.ITEM_ONE:
+                ViewHolderDefault viewHolderDefault  = (ViewHolderDefault) holder;
+                viewHolderDefault.bind(diamond);
+                viewHolderDefault.imageViewDiamond.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DiamondDetailsFragment fragment = new DiamondDetailsFragment();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("RECORD_NUMBER", diamond.getKey());
+                        fragment.setArguments(bundle);
+                        loadFragment(fragment);
+                    }
+                });
+                break;
+
+            case Constants.ITEM_TWO:
+                ViewHolder viewHolder = (ViewHolder) holder;
+                viewHolder.bind(diamond);
+                viewHolder.imageViewIcon.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DiamondDetailsFragment fragment = new DiamondDetailsFragment();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("RECORD_NUMBER", diamond.getKey());
+                        fragment.setArguments(bundle);
+                        loadFragment(fragment);
+                    }
+                });
+                break;
+        }
+
+
     }
 
     private void loadFragment(Fragment fragment) {
@@ -63,7 +96,16 @@ public class RecordsAdapter extends RecyclerView.Adapter<RecordsAdapter.ViewHold
         return diamondList.size();
     }
 
-    public static class  ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return Constants.ITEM_ONE;
+        } else {
+            return Constants.ITEM_TWO;
+        }
+    }
+
+    public static class ViewHolderDefault extends RecyclerView.ViewHolder {
         @BindView(R.id.tv_holder_name)
         TextView holderName;
 
@@ -73,7 +115,7 @@ public class RecordsAdapter extends RecyclerView.Adapter<RecordsAdapter.ViewHold
         @BindView(R.id.iv_diamond)
         ImageView imageViewDiamond;
 
-        public ViewHolder(View view) {
+        public ViewHolderDefault(View view) {
             super(view);
             ButterKnife.bind(this, view);
         }
@@ -82,6 +124,33 @@ public class RecordsAdapter extends RecyclerView.Adapter<RecordsAdapter.ViewHold
             holderName.setText(diamond.getRecord().getName());
             size.setText("#" + diamond.getKey());
             Picasso.with(itemView.getContext()).load(R.drawable.diamond_placeholder).into(imageViewDiamond);
+        }
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        String TAG = RecordsAdapter.class.getSimpleName();
+        @BindView(R.id.tv_diamond_item_name)
+        TextView tvDiamondName;
+
+        @BindView(R.id.tv_diamond_id)
+        TextView tvDimondId;
+
+        @BindView(R.id.iv_circle)
+        ImageView imageViewIcon;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+        public void bind(Diamond diamond) {
+            Log.d(TAG, "bind: " + diamond.getRecord().getName());
+            tvDiamondName.setText(diamond.getRecord().getName());
+            tvDimondId.setText("#" + diamond.getKey());
+            TextDrawable drawable = TextDrawable.builder()
+                    .buildRound(Character.toString(diamond.getRecord().getName().charAt(0)), R.color.colorPrimaryDark);
+            imageViewIcon.setImageDrawable(drawable);
         }
     }
 }
--- app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/utils/Constants.java
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/utils/Constants.java
@@ -1,10 +1,11 @@
 package com.globant.akashdanao.hyperledgerdiamond.utils;
 
 public class Constants {
-    //    public static final String BASE_URL = "http://10.0.2.2:8000";
-    public static final String BASE_URL = "http://192.168.43.87:8000";
+        public static final String BASE_URL = "http://10.0.2.2:8000";
+//    public static final String BASE_URL = "http://192.168.43.87:8000";
     public static final String IGI = "IGI";
     public static final String GIA = "GIA";
     public static final String HRD = "HRD";
-
+    public static final int ITEM_ONE = 1;
+    public static final int ITEM_TWO = 2;
 }
