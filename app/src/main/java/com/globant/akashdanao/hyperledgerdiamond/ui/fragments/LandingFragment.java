131fb3b603fb72b080f6ec8db1fa40d03644641a
AkashDanao <akash.danao@globant.com>
2018-03-12 14:02:21 +0530

    add app icon landing fragment design fix

--- app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/fragments/LandingFragment.java
+++ app/src/main/java/com/globant/akashdanao/hyperledgerdiamond/ui/fragments/LandingFragment.java
@@ -20,6 +20,7 @@ import com.globant.akashdanao.hyperledgerdiamond.ui.adapters.RecordsAdapter;
 import com.globant.akashdanao.hyperledgerdiamond.utils.PicassoCircleTransformation;
 import com.squareup.picasso.Picasso;
 
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
